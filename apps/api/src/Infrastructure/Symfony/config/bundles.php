18ead7b5f772e8e65fff70502a6fb013bc578ef0
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2022-08-22 17:37:23 +0200

    chore: add Symfony profiler

--- apps/api/src/Infrastructure/Symfony/config/bundles.php
+++ apps/api/src/Infrastructure/Symfony/config/bundles.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
 ];
