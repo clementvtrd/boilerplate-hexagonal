0ec6182d33ac2739db914ee585518252316ddc85
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2022-08-24 15:01:16 +0200

    chore: move GQL files to a dedicated directory

--- apps/frontend/src/hooks/graphql.ts
+++ apps/frontend/src/hooks/graphql.ts
@@ -1,48 +1,48 @@
-import { gql } from '@apollo/client'
-import * as Apollo from '@apollo/client'
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { readonly [key: string]: unknown }> = { readonly [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { readonly [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { readonly [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions = {} as const
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  readonly ID: string;
-  readonly String: string;
-  readonly Boolean: boolean;
-  readonly Int: number;
-  readonly Float: number;
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
 };
 
 export type Identifiable = {
-  readonly id: Scalars['ID'];
+  id: Scalars['ID'];
 };
 
 export type Query = {
-  readonly __typename?: 'Query';
-  readonly todoLists: ReadonlyArray<TodoList>;
+  __typename?: 'Query';
+  todoLists: Array<TodoList>;
 };
 
 export type Todo = Identifiable & {
-  readonly __typename?: 'Todo';
-  readonly id: Scalars['ID'];
-  readonly isDone: Scalars['Boolean'];
-  readonly task: Scalars['String'];
+  __typename?: 'Todo';
+  id: Scalars['ID'];
+  isDone: Scalars['Boolean'];
+  task: Scalars['String'];
 };
 
 export type TodoList = Identifiable & {
-  readonly __typename?: 'TodoList';
-  readonly id: Scalars['ID'];
-  readonly length: Scalars['Int'];
-  readonly title: Scalars['String'];
-  readonly todos: ReadonlyArray<Todo>;
+  __typename?: 'TodoList';
+  id: Scalars['ID'];
+  length: Scalars['Int'];
+  title: Scalars['String'];
+  todos: Array<Todo>;
 };
 
-export type TodoListsQueryVariables = Exact<{ readonly [key: string]: never; }>;
+export type TodoListsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type TodoListsQuery = { readonly __typename?: 'Query', readonly todoLists: ReadonlyArray<{ readonly __typename?: 'TodoList', readonly id: string, readonly title: string, readonly length: number, readonly todos: ReadonlyArray<{ readonly __typename?: 'Todo', readonly id: string, readonly task: string, readonly isDone: boolean }> }> };
+export type TodoListsQuery = { __typename?: 'Query', todoLists: Array<{ __typename?: 'TodoList', id: string, title: string, length: number, todos: Array<{ __typename?: 'Todo', id: string, task: string, isDone: boolean }> }> };
 
 
 export const TodoListsDocument = gql`
@@ -58,7 +58,7 @@ export const TodoListsDocument = gql`
     }
   }
 }
-    `
+    `;
 
 /**
  * __useTodoListsQuery__
@@ -76,13 +76,13 @@ export const TodoListsDocument = gql`
  * });
  */
 export function useTodoListsQuery(baseOptions?: Apollo.QueryHookOptions<TodoListsQuery, TodoListsQueryVariables>) {
-  const options = {...defaultOptions, ...baseOptions}
-  return Apollo.useQuery<TodoListsQuery, TodoListsQueryVariables>(TodoListsDocument, options)
-}
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<TodoListsQuery, TodoListsQueryVariables>(TodoListsDocument, options);
+      }
 export function useTodoListsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<TodoListsQuery, TodoListsQueryVariables>) {
-  const options = {...defaultOptions, ...baseOptions}
-  return Apollo.useLazyQuery<TodoListsQuery, TodoListsQueryVariables>(TodoListsDocument, options)
-}
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<TodoListsQuery, TodoListsQueryVariables>(TodoListsDocument, options);
+        }
 export type TodoListsQueryHookResult = ReturnType<typeof useTodoListsQuery>;
 export type TodoListsLazyQueryHookResult = ReturnType<typeof useTodoListsLazyQuery>;
 export type TodoListsQueryResult = Apollo.QueryResult<TodoListsQuery, TodoListsQueryVariables>;
--- apps/frontend/src/index.ts
+++ apps/frontend/src/index.ts
@@ -4,5 +4,4 @@ import App from 'components/App'
 
 import 'styles/index.scss'
 
-// eslint-disable-next-line functional/no-expression-statement
 render(createElement(App), document.getElementById('root'))
