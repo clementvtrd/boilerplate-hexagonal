77650ec75dc027d72fe998b59c82c614924abb53
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2022-10-15 15:07:57 +0200

    chore: add abstract repositories

---
+++ apps/api/src/Domain/Repository/TodoListRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Repository;
+
+/**
+ * @extends AbstractRepository<\Domain\Model\TodoList>
+ */
+abstract class TodoRepository extends AbstractRepository
+{
+}
---
+++ apps/api/src/Domain/Repository/TodoRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Repository;
+
+/**
+ * @extends AbstractRepository<\Domain\Model\Todo>
+ */
+abstract class TodoRepository extends AbstractRepository
+{
+}
