ccac0a24fc80edac1ac4004922d5be40fbaf1103
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2023-04-20 23:06:41 +0200

    chore: remove all opinionated content

--- apps/web/src/components/Main.tsx
+++ apps/web/src/components/Main.tsx
@@ -1,24 +1,7 @@
-import { useQuery } from '@apollo/client'
-import { UsersDocument, UsersQuery, UsersQueryVariables } from '@/graphql'
-
-const Main = () => {
-  const { data, loading } = useQuery<UsersQuery, UsersQueryVariables>(UsersDocument)
-
-  if (loading)
-    return (
-      <main>
-        <b>Loading</b>
-      </main>
-    )
-
+export default function Main() {
   return (
     <main>
       <h1>Hello world</h1>
-      {
-        data.users.map(user => <p key={user.id}>{`User ID: ${user.id}`}</p>)
-      }
     </main>
   )
 }
-
-export default Main
