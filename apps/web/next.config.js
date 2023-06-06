4931fb3ce4b287e3082f3e2d0eb47a8689a7d200
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2023-06-06 23:30:07 +0200

    chore: bootstrap Next JS frontend

---
+++ apps/web/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    devIndicators: {
+        buildActivity: true,
+    },
+    buildActivityPosition: 'bottom-right',
+    typedRoutes: true,
+
+}
+
+module.exports = nextConfig
--- apps/web/src/components/Main.tsx
+++
@@ -1,7 +0,0 @@
-export default function Main() {
-  return (
-    <main>
-      <h1>Hello world</h1>
-    </main>
-  )
-}
