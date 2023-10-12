ff049b0389d9ab4a881b845a343630bc2284152d
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2023-10-12 17:12:52 +0200

    chore: delete legacy react app

--- apps/legacy/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createElement } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import '@/styles/index.scss'
-import App from '@/components/App'
-
-const root = createRoot(document.getElementById('root'))
-root.render(createElement(App))
