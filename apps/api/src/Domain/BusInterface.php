13771d58a9f65ad97246156d4fbae630080fb47f
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2024-01-24 22:39:36 +0100

    chore: run php cs fixer

--- apps/api/src/Domain/BusInterface.php
+++ apps/api/src/Domain/BusInterface.php
@@ -9,6 +9,8 @@ use Domain\Query\QueryInterface;
 interface BusInterface
 {
     public function ask(QueryInterface $query): mixed;
+
     public function dispatch(CommandInterface $command): void;
+
     public function emit(EventInterface $event): void;
 }
