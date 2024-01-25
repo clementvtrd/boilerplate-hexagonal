5579203f6009a5bae13dda2dfc7e937d8670bcf2
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2024-01-25 22:15:57 +0100

    chore: remove GraphQL related files and dependencies

--- apps/api/config/bundles.php
+++ apps/api/config/bundles.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
